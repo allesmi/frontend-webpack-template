9ee54387033a5785b40759e63b2aa6ddca10a367
Alexander Miller <alles.mil@gmail.com>
2020-10-30 18:38:31 +0100

    Add babel to build process

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -14,14 +14,24 @@ module.exports = {
     })
   ],
   module: {
-      rules: [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader'
-              ]
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
-      ]
+        }
+      }
+    ]
   }
 };
